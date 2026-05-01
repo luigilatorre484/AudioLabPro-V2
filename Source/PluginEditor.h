106d69e2097529e34a87e582a018431075157a47
luigilatorre484 <luigilatorre484@gmail.com>
2026-05-01 17:22:40 +0200

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -2,17 +2,28 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-class AudioLabProAudioProcessorEditor : public juce::AudioProcessorEditor
+class AudioLabProAudioProcessorEditor  : public juce::AudioProcessorEditor, private juce::Timer
 {
 public:
     AudioLabProAudioProcessorEditor (AudioLabProAudioProcessor&);
     ~AudioLabProAudioProcessorEditor() override;
+
     void paint (juce::Graphics&) override;
     void resized() override;
+    void timerCallback() override;
 
 private:
     AudioLabProAudioProcessor& audioProcessor;
-    juce::Slider gainSlider;
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainAttachment;
+
+    juce::Image backgroundImage;
+    juce::TextButton analyzeButton;
+    juce::TextButton bypassButton;
+    juce::ComboBox genreSelector;
+    
+    juce::Slider loudnessSlider;
+    juce::Slider widthSlider;
+    juce::Slider toneKnob;
+    juce::Slider driveKnob;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioLabProAudioProcessorEditor)
 };
