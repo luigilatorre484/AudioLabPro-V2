3f7429f3d04520701786b5e7260e612ff9730ddd
luigilatorre484 <luigilatorre484@gmail.com>
2026-05-01 14:57:40 +0200

    Create PluginProcessor.h

---
+++ .github/workflows/Source/PluginProcessor.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <JuceHeader.h>
+
+class AudioLabProAudioProcessor : public juce::AudioProcessor
+{
+public:
+    AudioLabProAudioProcessor();
+    ~AudioLabProAudioProcessor() override;
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override { return true; }
+
+    const juce::String getName() const override { return "AudioLabPro V2"; }
+    bool acceptsMidi() const override { return false; }
+    bool producesMidi() const override { return false; }
+    double getTailLengthSeconds() const override { return 0.0; }
+
+    int getNumPrograms() override { return 1; }
+    int getCurrentProgram() override { return 0; }
+    void setCurrentProgram (int index) override {}
+    const juce::String getProgramName (int index) override { return {}; }
+    void changeProgramName (int index, const juce::String& newName) override {}
+
+    void getStateInformation (juce::MemoryBlock& destData) override {}
+    void setStateInformation (const void* data, int sizeInBytes) override {}
+
+    juce::AudioProcessorValueTreeState& getTreeState() { return treeState; }
+
+private:
+    juce::AudioProcessorValueTreeState treeState;
+    juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioLabProAudioProcessor)
+};
