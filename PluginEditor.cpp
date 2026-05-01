3ed6692de783e0fcad86dafeb29d53b5363815d9
luigilatorre484 <luigilatorre484@gmail.com>
2026-05-01 15:13:49 +0200

    Create PluginEditor.cpp

---
+++ PluginEditor.cpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+class AudioLabProAudioProcessorEditor : public juce::AudioProcessorEditor
+{
+public:
+    AudioLabProAudioProcessorEditor (AudioLabProAudioProcessor&);
+    ~AudioLabProAudioProcessorEditor() override;
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    AudioLabProAudioProcessor& audioProcessor;
+    juce::Slider gainSlider;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainAttachment;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioLabProAudioProcessorEditor)
+};
